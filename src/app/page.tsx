c905cd7804639361bffdb0181c54398efe575e0a
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-03-11 21:37:25 +0900

    fix: Github pagesへデプロイするため、静的サイトを出力するように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+// @ts-ignore
+import { basePath } from "../../next.config";
+const BASE_PATH = basePath ? basePath : "";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -17,7 +21,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src={`${BASE_PATH}/vercel.svg`}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -31,7 +35,7 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src={`${BASE_PATH}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
