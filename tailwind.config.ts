b2a0531d3590b63a216ddcf87a987e2f78eb8c57
kana <144141297+kana1166@users.noreply.github.com>
2024-03-14 23:21:33 +0900

    style: Inter, Noto Sans Japaneseを設定、lang属性をjaに変更
    * fix/font
    
    * fix/notosanjp
    
    * fix/inter
    
    * style: Inter, Noto Sans Japaneseを設定、lang属性をjaに変更
    
    ---------
    
    Co-authored-by: Aileen <140990100+pss-aileen@users.noreply.github.com>
    
    closed #31

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
