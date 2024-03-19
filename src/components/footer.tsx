71fb9b910ec9e74823e9be3a65d7d36aafba5df1
kagome <fu.taz.1102@gmail.com>
2024-03-19 19:04:48 +0900

    🐛 fix: paddingの追加

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-red-600 text-white text-center">
+    <footer className="bg-red-600 text-white text-center py-2">
       <small>&copy; first-contributions-ja</small>
     </footer>
   )
