0ee18cc2a2d4873261d2b75e4a71372142b0ff2d
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-03-28 18:55:18 +0800

    🎨 style: コントリビューション概要仮実装 #64

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { basePath } from "../../next.config";
 const BASE_PATH = basePath ? basePath : "";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { SITE_NAME } from "../../lib/constants";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import ForkRightIcon from "@mui/icons-material/ForkRight";
 
 export default function Home() {
   return (
@@ -35,34 +37,123 @@ export default function Home() {
           />
         </div>
       </section>
-      <section className="bg-red-600 px-10 py-12">
-        <div className="rounded-xl bg-white p-20">
-          <h2 className="text-center text-4xl font-bold tracking-tighter text-red-600">
-            簡単７ステップでコントリビューション！
+      <section className="bg-red-600 p-16">
+        <div className="rounded-lg bg-white p-16 ">
+          <p className="text-center text-sm font-bold">＼ TRY THIS NOW ／</p>
+          <h2 className="mt-2 justify-center gap-2 text-center text-2xl font-bold text-red-600">
+            <span>8 STEP で学ぶ OSS コントリビューション </span>
+            <span className="text-3xl">🫶</span>
           </h2>
-          <ol className="mx-auto mb-8 mt-10 max-w-[500px] list-inside list-decimal text-2xl font-bold leading-loose">
-            <li>プロジェクトをフォークする</li>
-            <li>ローカルリポジトリにクローンする</li>
-            <li>ブランチを作成する</li>
-            <li>ソースコードを変更！</li>
-            <li>コミットメッセージを添えてコミットする</li>
-            <li>コミット内容をプッシュする</li>
-            <li>プルリクエストを作成する</li>
-          </ol>
-          <div className="mb-16 text-center text-2xl font-bold leading-relaxed">
-            <p className="mb-8 text-4xl text-red-600">⬇︎</p>
-            <p>マージされると……</p>
-            <p>🎉 あなたの変更がメインプロジェクトに反映されます 🎉</p>
+          {/* <ol className="mx-auto mt-8 w-fit list-inside list-decimal leading-loose"> */}
+          <div className="mx-auto mt-12 flex w-fit justify-center gap-12 rounded-md  bg-zinc-50 p-8">
+            <ol className=" w-fit font-bold leading-loose">
+              <li>
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">🎁</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold ">STEP 1</span>
+                    リポジトリをフォーク
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">⬇️</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 2</span>
+                    ローカルマシンへクローン
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">🌳</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 3</span>
+                    作業用ブランチを作成
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">🔥</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 4</span>
+                    変更を加える
+                  </p>
+                </div>
+              </li>
+            </ol>
+            <ol className=" w-fit font-bold leading-loose">
+              <li>
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">🚧</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 5</span>
+                    変更をコミット
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">🎈</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 6</span>
+                    変更をプッシュ
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">💌</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 7</span>
+                    プルリクエスト（PR）を作成
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6">
+                <div className="flex items-center gap-4">
+                  <div className=" flex aspect-square w-[56px] items-center justify-center rounded bg-white text-2xl shadow-lg">
+                    <span className="drop-shadow-lg">📝</span>
+                  </div>
+                  <p className="flex-1">
+                    <span className="block text-[12px] font-bold">STEP 8</span>
+                    レビューとフィードバックに対応
+                  </p>
+                </div>
+              </li>
+            </ol>
           </div>
-          <div className="text-center">
-            <a
-              href="https://github.com/first-contributions-ja/first-contributions-ja.github.io"
-              className="rounded-xl bg-red-600 px-5 py-4 text-lg text-white transition hover:opacity-70"
-            >
-              <GitHubIcon fontSize="large" className="mr-3 -translate-y-0.5" />
-              詳しい手順はこちら
-            </a>
-          </div>
+          <p className="mt-4 text-center text-red-600">
+            <KeyboardDoubleArrowDownIcon className="text-4xl" />
+          </p>
+          <p className="mt-4 text-center text-lg font-bold">
+            マージされて...Finish 🥳
+            <br />
+            あなたの変更がメインプロジェクトに反映されます 🎉
+          </p>
+          <a
+            href="https://github.com/first-contributions-ja/first-contributions-ja.github.io"
+            className="mx-auto mt-8 flex w-fit items-center justify-center gap-2 rounded-lg bg-red-600 px-4 py-3 text-white transition hover:opacity-70"
+          >
+            <GitHubIcon fontSize="medium" className="" />
+            <span>詳しい手順はこちら</span>
+          </a>
         </div>
       </section>
     </>
