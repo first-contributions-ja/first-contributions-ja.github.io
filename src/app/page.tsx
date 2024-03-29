4c538af1859861df82d752c729cd2f124bc5495a
kagome <fu.taz.1102@gmail.com>
2024-03-29 15:11:51 +0900

    🐛 fix: 最終的なスタイルの微調整、コード内の不要な空白の削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import KeyboardDoubleArrowDownSharpIcon from "@mui/icons-material/KeyboardDouble
 export default function Home() {
   return (
     <>
-      <section className=" mx-auto mt-20 max-w-2xl px-6 lg:-mb-4 lg:mt-4 lg:flex lg:h-screen lg:max-w-7xl lg:items-center  lg:gap-10">
+      <section className="mx-auto mt-20 max-w-2xl px-6 lg:-mb-4 lg:mt-4 lg:flex lg:h-screen lg:max-w-7xl lg:items-center  lg:gap-10">
         <div className="lg:flex-1">
           <p className="hidden bg-stone-100 px-4 py-2 lg:inline-block">
             <span className="text-2xl font-bold text-red-600">1234</span>
@@ -56,50 +56,50 @@ export default function Home() {
         </p>
       </section>
 
-      <section className="bg-red-600 p-6 pb-0  pt-8 md:px-10 md:pt-10">
+      <section className="bg-red-600 p-6 pb-0 pt-8 md:px-10 md:pt-10">
         <div className="rounded-md bg-white px-5 pb-16 pt-12 md:p-20 md:pt-8">
           <h2 className="text-center text-xl font-bold tracking-tighter text-red-600 md:pb-4 md:pt-12 md:text-3xl">
             簡単 8 STEP でコントリビューション！
           </h2>
-          <ul className="mx-auto mt-8 w-fit list-inside rounded-md px-2 text-lg font-bold md:text-xl md:leading-loose lg:mb-8 lg:mt-10 lg:flex lg:gap-16 lg:bg-stone-100 lg:px-12 lg:pb-4 lg:pt-8">
+          <ul className="mx-auto mt-8 w-fit list-inside rounded-md px-2 text-lg font-bold md:text-xl md:leading-7 lg:mb-8 lg:mt-10 lg:flex lg:gap-16 lg:bg-stone-100 lg:px-12 lg:pb-4 lg:pt-8">
             <div>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 1</p>
                 プロジェクトをフォーク
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 2</p>
                 ローカルマシンへクローン
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 3</p>
                 作業用ブランチを作成
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 4</p>
                 変更を加える
               </li>
             </div>
             <div>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 5</p>
                 変更をコミット
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 6</p>
                 変更をプッシュ
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 7</p>
                 プルリクエストを作成
               </li>
-              <li className="pb-4">
+              <li className="pb-4 md:pb-6">
                 <p className="mr-6 text-sm text-red-600 md:text-lg">STEP 8</p>
                 レビューとフィードバックに対応
               </li>
             </div>
           </ul>
-          <div className="mb-8 text-center font-bold leading-7 md:text-xl md:leading-loose">
+          <div className="mb-8 text-center font-bold leading-7 md:text-xl md:leading-8">
             <KeyboardDoubleArrowDownSharpIcon className="mb-4 text-6xl text-red-600" />
             <p>プルリクエストが承認されると</p>
             <p>あなたの変更がメインプロジェクトに反映されます 🎉</p>
@@ -107,9 +107,9 @@ export default function Home() {
           <div className="text-center">
             <a
               href="https://github.com/first-contributions-ja/first-contributions-ja.github.io"
-              className="rounded-md bg-red-600 px-5 py-4 text-white transition hover:opacity-70 lg:text-lg"
+              className="rounded-md bg-red-600 px-3 py-3 text-sm text-white transition hover:opacity-70 md:px-5 md:py-4 md:text-lg"
             >
-              <GitHubIcon fontSize="large" className="mr-3 -translate-y-0.5" />
+              <GitHubIcon className="mr-3 -translate-y-0.5 text-3xl md:text-4xl" />
               詳しい手順はこちら
             </a>
           </div>
