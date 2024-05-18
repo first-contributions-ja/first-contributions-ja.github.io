34d8e48eb391fbfdab360b5804519e7169c81381
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-05-19 07:30:01 +0900

    🎨 style: 背景グラデーションの実装 #185

---
+++ src/utils/background-color.ts
@@ -0,0 +1,21 @@
+const hexToRgb = (color: string, transparency = 1) => {
+  const rgbValues = Object.fromEntries(
+    (
+      (color.match(/^#?[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?$/) ? color : "000")
+        .replace(/^#?(.*)$/, (_, hex) =>
+          hex.length == 3 ? hex.replace(/./g, "$&$&") : hex,
+        )
+        .match(/../g) ?? []
+    ).map((c, i) => ["rgb".charAt(i), parseInt("0x" + c)]),
+  );
+
+  return `rgb(${rgbValues.r} ${rgbValues.g} ${rgbValues.b} / ${transparency})`;
+};
+
+export const getGradientStyles = (color: string) => {
+  return {
+    "--background-start-rgb": hexToRgb(color, 0.05),
+    "--background-middle-rgb": hexToRgb(color, 0.4),
+    "--background-end-rgb": hexToRgb(color),
+  };
+};
