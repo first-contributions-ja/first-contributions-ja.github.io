e1bac90c439d12c9237a3f9417fb54e7c88378ce
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-06-09 02:17:40 +0900

    :bug: fix: 絵文字が境界をはみ出す不具合を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" className="overflow-x-hidden">
       <body
         className={`${inter.variable} ${notojp.variable} overflow-x-hidden text-stone-800`}
       >
